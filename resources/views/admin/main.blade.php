46b3c3b787fe125475e0a8a952e75961064f6cec
fengye712 <1419079467@qq.com>
2016-06-17 08:26:56 +0800

    view update Signed-off-by: fengye712 <1419079467@qq.com>

--- resources/views/admin/main.blade.php
+++ resources/views/admin/main.blade.php
@@ -1,4 +1,4 @@
-﻿<body>
+﻿
 <frameset rows="88,*" cols="*" frameborder="no" border="1" framespacing="0">
   <frame src="top" name="topFrame" scrolling="No" noresize="noresize" id="topFrame" title="topFrame" />
   <frameset cols="187,*" frameborder="no" border="1" framespacing="0">
@@ -6,4 +6,3 @@
     <frame src="index" name="rightFrame" id="rightFrame" title="rightFrame" />
   </frameset>
 </frameset>
-</body>
