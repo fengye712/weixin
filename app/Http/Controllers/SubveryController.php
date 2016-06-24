382d186c8c245f2a2b2605d5c5f5d09ab713b241
fengye712 <1419079467@qq.com>
2016-06-24 18:07:09 +0800

    sfasf
    Signed-off-by: fengye712 <1419079467@qq.com>

--- app/Http/Controllers/SubveryController.php
+++ app/Http/Controllers/SubveryController.php
@@ -13,40 +13,40 @@ class SubveryController extends Controller
 	//新建调查表
 	public function index(){
 		if ($_POST) {
-			$s=new Subvery;
-			//接收数据
-			$s->s_title=Request::get('sub_title');
-			$s->s_desc=Request::get('sub_content');
-			$s->s_object=Request::get('"m_object');
-			$s->s_type=Request::get('m_type');
+			// $s=new Subvery;
+			// //接收数据
+			// $s->s_title=Request::get('sub_title');
+			// $s->s_desc=Request::get('sub_content');
+			// $s->s_object=Request::get('"m_object');
+			// $s->s_type=Request::get('m_type');
 			
-			$s->s_endtime=Request::get('m_endtime');
+			// $s->s_endtime=Request::get('m_endtime');
 			
-                               $sub_item=Request::get('m_item');
-                              
-                            
-			if ($s->save()) {
-				$sid=$s->Id;
-				$count=count($sub_item);
-				for($i=0;$i<$count;$i++)
-				{
-				$sql="insert into subitem(s_item,sid) value('$sub_item[$i]',$sid)";
-				$res=DB::insert($sql);
-				
-				}
-				
-				if ($res) {
-                                                    echo "<script>alert('添加成功')</script>";
-				}else{
-					echo "<script>alert('添加失败')</script>";
-					echo Redirect::action("SubveryController@index");	
-				}
+   //                             $sub_item=Request::get('m_item');
 
-			}else{
-				echo "<script>alert('添加失败')</script>";
-				echo Redirect::action("SubveryController@index");
-			}
-			
+                            
+			// if ($s->save()) {
+			// 	$sid=$s->Id;
+			// 	$count=count($sub_item);
+			// 	for($i=0;$i<$count;$i++)
+			// 	{
+			// 	$sql="insert into subitem(s_item,sid) value('$sub_item[$i]',$sid)";
+			// 	$res=DB::insert($sql);
+				
+			// 	}
+				
+			// 	if ($res) {
+   //                                                  echo "<script>alert('添加成功')</script>";
+			// 	}else{
+			// 		echo "<script>alert('添加失败')</script>";
+			// 		echo Redirect::action("SubveryController@index");	
+			// 	}
+
+			// }else{
+			// 	echo "<script>alert('添加失败')</script>";
+			// 	echo Redirect::action("SubveryController@index");
+			// }
+			dd($_POST);
 
 		}else{
 			//$code=$_GET['code'];
