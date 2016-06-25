37e65b2a424262707be59a331fe6186cc460a487
fengye712 <1419079467@qq.com>
2016-06-25 09:39:37 +0800

    fhfghfgh
    Signed-off-by: fengye712 <1419079467@qq.com>

--- app/Http/Controllers/SubveryController.php
+++ app/Http/Controllers/SubveryController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Basic;
-use App\model\Subvery;
+//use App\model\Subvery;
 use Session,Cookie,DB,Memcache,Input,Redirect,Validator,IsValid,Request;
 
 class SubveryController extends Controller
@@ -13,32 +13,32 @@ class SubveryController extends Controller
 	//新建调查表
 	public function index(){
 		if ($_POST) {
-			$s=new Subvery;
+			//$s=new Subvery;
 			// //接收数据
 			ini_set("display_errors","On");
-			$s->s_title=Request::get('sub_title');
-			$s->s_desc=Request::get('sub_content');
-			$s->s_object=Request::get('"m_object');
-			$s->s_type=Request::get('m_type');
+			$s['s_title']=Request::get('sub_title');
+			$s['s_desc']=Request::get('sub_content');
+			$s['s_object']=Request::get('"m_object');
+			$s['s_type']=Request::get('m_type');
 			
-			$s->s_endtime=Request::get('m_endtime');
+			$s['s_endtime']=Request::get('m_endtime');
 			
                                $sub_item=Request::get('m_item');
-                               //  print_r($_POST);
+                             //  print_r($_POST);
                               // dd($s);
-                              // $res=DB::table('subvery')->insertGetId( $s );
+                              $res=DB::table('subvery')->insertGetId( $s );
                               //  $res=Subvery::addSub($s);
-			 if ($s->save()) {
-				$sid=$s->Id;
+			 if ($res>0) {
+				$sid=$res;
 				$count=count($sub_item);
 				for($i=0;$i<$count;$i++)
 				{
 				$sql="insert into subitem(s_item,sid) value('$sub_item[$i]',$sid)";
-				$res=DB::insert($sql);
+				$r=DB::insert($sql);
 				
 				}
 				
-				if ($res) {
+				if ($r) {
                                                     echo "<script>alert('添加成功')</script>";
 				}else{
 					echo "<script>alert('添加失败')</script>";
